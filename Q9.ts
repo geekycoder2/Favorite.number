a9557a37f33d99eaa4d7ca3da685ea281c1ec5b2
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-17 00:28:36 +0500

    Add files via upload

---
+++ Q9.ts
@@ -0,0 +1,6 @@
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNumber: number = 11
+let message:String=`My Favorite Number is ${favoriteNumber}`
+console.log(message)
+
